64e468c8c8606978dd34e670ca88af8c26ae38d3
Shubham Mohanty <101620532+ShubhamMohanty680@users.noreply.github.com>
2023-08-25 18:52:05 +0530

    Update model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -155,13 +155,6 @@ class ModelTrainer:
 
             logging.info(f"Extracting model config file path")
 
-
-            
-
-
-
-            logging.info(f"Extracting model config file path")
-
             model_report: dict = self.evaluate_models(X=x_train, y=y_train, models=self.models)
 
             ## To get best model score from dict
